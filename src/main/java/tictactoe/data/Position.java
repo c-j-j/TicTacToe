fa38d0c9bbaf86d025085021bb5aafff646c9495
chrisjordan87 <chrisjordan1987@gmail.com>
2014-09-03 15:40:20 +0100

    Removing position array

--- src/main/java/tictactoe/data/Position.java
+++ src/main/java/tictactoe/data/Position.java
@@ -2,19 +2,8 @@ package tictactoe.data;
 
 public enum Position
 {
-    TOP_LEFT(0), TOP_CENTRE(1), TOP_RIGHT(2),
-    MIDDLE_LEFT(3), CENTRE(4), MIDDLE_RIGHT(5),
-    BOTTOM_LEFT(6), BOTTOM_CENTRE(7), BOTTOM_RIGHT(8);
+    TOP_LEFT, TOP_CENTRE, TOP_RIGHT,
+    MIDDLE_LEFT, CENTRE, MIDDLE_RIGHT,
+    BOTTOM_LEFT, BOTTOM_CENTRE, BOTTOM_RIGHT;
 
-    private final int arrayPosition;
-
-    Position(int arrayPosition)
-    {
-        this.arrayPosition = arrayPosition;
-    }
-
-    public int getArrayPosition()
-    {
-        return arrayPosition;
-    }
 }
